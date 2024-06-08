fe2b91730206ee24a3b7b9e2898e3d3d5d9575cf
an3347 <akshat.nagpal6@gmail.com>
2024-06-08 13:29:20 -0400

    my fun project - create a stationery

---
+++ generate_lined_file.py
@@ -0,0 +1,16 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation="P", unit="mm", format="letter")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+for k in range(2):
+    pdf.add_page()
+    i = 18
+    while i <= 275:
+        pdf.line(0, i, 216, i)
+        i += 9
+    i -= 10
+
+    pdf.line(27, 0, 27, 280)
+
+pdf.output("text_file.pdf")
