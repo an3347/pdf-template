0d7a545d631219b9fa52a25427d883ef67f809fd
an3347 <akshat.nagpal6@gmail.com>
2024-08-25 00:51:20 -0400

    Add lines

--- generate_lined_file.py
+++ generate_lined_file.py
@@ -5,12 +5,12 @@ pdf.set_auto_page_break(auto=False, margin=0)
 
 for k in range(2):
     pdf.add_page()
-    i = 18
+    i = 16
     while i <= 275:
         pdf.line(0, i, 216, i)
-        i += 9
+        i += 8
     i -= 10
 
     pdf.line(27, 0, 27, 280)
 
-pdf.output("text_file.pdf")
+pdf.output("text_file_2.pdf")
